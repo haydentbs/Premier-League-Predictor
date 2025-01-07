110cf38aa0967409d24dd2e10c3833123ba2371b
haydentbs <hayden.tibbals@gmail.com>
2025-01-07 12:10:20 +0000

    features good

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -70,7 +70,7 @@ class Scraper:
         df['A.xg'] = df['xG.1']
         df['H.xga'] = df['xG.1']
         df['A.xga'] = df['xG']
-        df['Result'] = df['Score'].apply(lambda x: None if pd.isna(x) else 2 if x.split('–')[0] > x.split('–')[1] else 1 if x.split('–')[0] == x.split('–')[1] else 0)
+        df['Result'] = df['Score'].apply(lambda x: None if pd.isna(x) else 3 if x.split('–')[0] > x.split('–')[1] else 1 if x.split('–')[0] == x.split('–')[1] else 0)
         
 
         features = ['Date', 'Day','Time', 'Result', 'H.team', 'A.team', 'H.xg', 'A.xg', 'H.xga', 'A.xga', 'Status']
@@ -78,61 +78,67 @@ class Scraper:
         return df[features]
 
 
-def feature_engineering(df):
+    def feature_engineering(self):
 
-    # Rolling Averages
-    home_df = df[['H.team', 'A.team','H.xg', 'H.xga', 'Date', 'Result', 'Status']]
-    away_df = df[['A.team', 'H.team','A.xg', 'A.xga', 'Date', 'Result', 'Status']]
-    away_df['Result_True'] = away_df['Result'].apply(lambda x: 2 if x ==0 else 1 if x==1 else 0)
-    away_df.drop(columns=['Result'], inplace=True)
+        df = self.process_data()
 
-    home_df.columns = ['Team', 'Opponent', 'xg', 'xga', 'Date', 'Result', 'Status']
-    away_df.columns = ['Team', 'Opponent', 'xg', 'xga', 'Date', 'Result', 'Status']
+        # Rolling Averages
+        home_df = df[['H.team', 'A.team','H.xg', 'H.xga', 'Date',  'Status' ,'Result']].copy()
+        away_df = df[['A.team', 'H.team','A.xg', 'A.xga', 'Date', 'Status', 'Result']].copy()
+        away_df['Result_True'] = away_df['Result'].apply(lambda x: 3 if x ==0 else 0 if x==3 else 1)
+        away_df.drop(columns=['Result'], inplace=True)
 
-    team_df = pd.concat([home_df, away_df])
+        home_df.columns = ['Team', 'Opponent', 'xg', 'xga', 'Date', 'Status', 'Result']
+        away_df.columns = ['Team', 'Opponent', 'xg', 'xga', 'Date', 'Status', 'Result']
+        home_df['location'] = 'home'
+        away_df['location'] = 'away'
 
-    team_df = team_df.groupby('Team').apply(lambda x: x.sort_values('Date'))
+        team_df = pd.concat([home_df, away_df])
 
-    team_df['rolling_xg'] = team_df['xg'].rolling(window=5, min_periods=1).mean()
-    team_df['rolling_xga'] = team_df['xga'].rolling(window=5, min_periods=1).mean()
+        team_df = team_df.sort_values(['Team', 'Date'])
+        team_df = team_df.groupby('Team', group_keys=False).apply(lambda x: x)
 
-    team_df['rolling_xg_diff'] = team_df['rolling_xg'] - team_df['rolling_xga']
+        team_df['rolling_xg'] = team_df['xg'].rolling(window=5, min_periods=1).mean()
+        team_df['rolling_xga'] = team_df['xga'].rolling(window=5, min_periods=1).mean()
 
-    team_df['form_rolling_5'] = team_df['Result'].rolling(window=5, min_periods=1).mean()
-    team_df['form_rolling_10'] = team_df['Result'].rolling(window=10, min_periods=1).mean()
+        team_df['rolling_xg_diff'] = team_df['rolling_xg'] - team_df['rolling_xga']
 
+        team_df['form_rolling_5'] = team_df['Result'].rolling(window=5, min_periods=1).mean()
+        team_df['form_rolling_10'] = team_df['Result'].rolling(window=10, min_periods=1).mean()
+
+        team_df['opponent_form_rolling_3'] = team_df['Result'].rolling(window=3, min_periods=1).mean()
+        team_df['opponent_form_rolling_6'] = team_df['Result'].rolling(window=6, min_periods=1).mean()
+
+        flat_df = team_df.reset_index()
+        flat_df = flat_df.sort_values(['Team', 'Opponent', 'location','Date'])
+        team_df = flat_df.groupby(['Team', 'Opponent', 'location'], 
+                            group_keys=False, 
+                            ).apply(lambda x: x)
+
+        team_df['opponent_home_form_rolling_2'] = team_df['Result'].rolling(window=2, min_periods=1).mean()
+        team_df['opponent_away_form_rolling_2'] = team_df['Result'].rolling(window=2, min_periods=1).mean()
+
+        team_df.reset_index( inplace=True)
+        team_df.sort_values(['Date', 'index'], inplace=True)
+
+
+        return team_df
     
-
-    flat_df = team_df.reset_index()
-    print(flat_df.head())
-    team_df = flat_df.groupby(['Team', 'Opponent'])
-
-    team_df['opponent_form_rolling_3'] = team_df['Result'].rolling(window=3, min_periods=1).mean()
-    team_df['opponent_form_rolling_6'] = team_df['Result'].rolling(window=6, min_periods=1).mean()
+    def get_data(self):
+        df = self.feature_engineering()
+        return df
 
 
 
-        # df['H.xg_rolling'] = df['H.xg'].rolling(window=5).mean()
-        # df['A.xg_rolling'] = df['A.xg'].rolling(window=5).mean()
-        # df['H.xga_rolling'] = df['H.xga'].rolling(window=5).mean()
-        # df['A.xga_rolling'] = df['A.xga'].rolling(window=5).mean()
-    
+scraper = Scraper([22,23,24])
+table = scraper.get_data()
+table.to_csv('premier_league_data.csv', index=False)
 
 
-    return team_df
-
-
-
-# scraper = Scraper([22,23,24])
-# table = scraper.process_data()
-# table.to_csv('premier_league_data.csv', index=False)
-
-table = pd.read_csv('premier_league_data.csv')
-
-
-
-table = feature_engineering(table)
+print(table.columns)
 
 print(table.head())
 print(table.tail())
+
+print(table[table['index']==0])
 
