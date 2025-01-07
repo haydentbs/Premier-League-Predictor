3d051949d87fa541429d03a373c1495ff3dc8c75
haydentbs <hayden.tibbals@gmail.com>
2025-01-07 10:50:47 +0000

    better scrape features

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -31,6 +31,8 @@ class Scraper:
         else:
             print(f"Failed to retrieve the page. Status code: {response.status_code}")
             return None
+        
+    
     
     def get_tables(self):
         # Method 1: Find table by ID
@@ -78,14 +80,52 @@ class Scraper:
 
 def feature_engineering(df):
 
+    # Rolling Averages
+    home_df = df[['H.team', 'A.team','H.xg', 'H.xga', 'Date', 'Result', 'Status']]
+    away_df = df[['A.team', 'H.team','A.xg', 'A.xga', 'Date', 'Result', 'Status']]
+    away_df['Result_True'] = away_df['Result'].apply(lambda x: 2 if x ==0 else 1 if x==1 else 0)
+    away_df.drop(columns=['Result'], inplace=True)
 
-    return df
+    home_df.columns = ['Team', 'Opponent', 'xg', 'xga', 'Date', 'Result', 'Status']
+    away_df.columns = ['Team', 'Opponent', 'xg', 'xga', 'Date', 'Result', 'Status']
+
+    team_df = pd.concat([home_df, away_df])
+
+    team_df = team_df.groupby('Team').apply(lambda x: x.sort_values('Date'))
+
+    team_df['rolling_xg'] = team_df['xg'].rolling(window=5, min_periods=1).mean()
+    team_df['rolling_xga'] = team_df['xga'].rolling(window=5, min_periods=1).mean()
+
+    team_df['rolling_xg_diff'] = team_df['rolling_xg'] - team_df['rolling_xga']
+
+    team_df['form_rolling_5'] = team_df['Result'].rolling(window=5, min_periods=1).mean()
+    team_df['form_rolling_10'] = team_df['Result'].rolling(window=10, min_periods=1).mean()
+
+    
+
+    flat_df = team_df.reset_index()
+    print(flat_df.head())
+    team_df = flat_df.groupby(['Team', 'Opponent'])
+
+    team_df['opponent_form_rolling_3'] = team_df['Result'].rolling(window=3, min_periods=1).mean()
+    team_df['opponent_form_rolling_6'] = team_df['Result'].rolling(window=6, min_periods=1).mean()
 
 
 
-scraper = Scraper([22,23,24])
-table = scraper.process_data()
-table.to_csv('premier_league_data.csv', index=False)
+        # df['H.xg_rolling'] = df['H.xg'].rolling(window=5).mean()
+        # df['A.xg_rolling'] = df['A.xg'].rolling(window=5).mean()
+        # df['H.xga_rolling'] = df['H.xga'].rolling(window=5).mean()
+        # df['A.xga_rolling'] = df['A.xga'].rolling(window=5).mean()
+    
+
+
+    return team_df
+
+
+
+# scraper = Scraper([22,23,24])
+# table = scraper.process_data()
+# table.to_csv('premier_league_data.csv', index=False)
 
 table = pd.read_csv('premier_league_data.csv')
 
